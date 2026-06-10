9a456f406a984675027beb80849f94d4117bb0cc
Zig3014 <zottistg@gmail.com>
2026-06-11 01:47:58 +0900

    6/10

---
+++ navLink.js
@@ -0,0 +1,10 @@
+const navHTML = `
+    <ul>
+        <a href="/index.html">ホーム</a>
+        <a href="/profile.html">自己紹介</a>
+        <a href="/documents.html">数学</a>
+        <a href="/diary/2026-06.html">日記</a>
+        <a href="/article.html">その他</a>
+    </ul>
+`;
+document.getElementById('navLink').innerHTML = navHTML;
