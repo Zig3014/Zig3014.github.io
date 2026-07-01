8b0e87fe13164e7c30196d29fc15c6b8b3c35f90
Zig3014 <zottistg@gmail.com>
2026-07-02 01:50:47 +0900

    7/1

--- navLink.js
+++ navLink.js
@@ -5,7 +5,7 @@ const navHTML = `
         <a href="/index.html">ホーム</a>
         <a href="/profile.html">自己紹介</a>
         <a href="/documents.html">数学</a>
-        <a href="/diary/2026-06.html">日記</a>
+        <a href="/diary/2026-07.html">日記</a>
         <a href="/article.html">その他</a>
     </ul>
 </header>
